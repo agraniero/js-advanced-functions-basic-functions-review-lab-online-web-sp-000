9466b0cd38daa325b970ba909fbd13b865104038
Anthony Graniero <ananfro@gmail.com>
2019-12-08 20:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function saturdayFun(activity='roller-skate'){
   return `This Saturday, I want to ${activity}!`;
-};
+}
 
 var mondayWork = function(activity='go to the office'){
   return `This Monday, I will ${activity}.`;
